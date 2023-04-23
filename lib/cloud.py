5d47fd4fe7bdf426f0a0017eca29e433bc13971b
Iyur <iyurrr@gmail.com>
2023-04-23 16:21:26 +0300

    Improvements

--- lib/cloud.py
+++ lib/cloud.py
@@ -1,13 +1,6 @@
-import random
-import os
-import requests
 import time
-import json
-from datetime import datetime
 from .db import DB
 from .managerapi import ManagerApi
-from .blobserverapi import BlobServerApi
-from .url import join_url, parse_url
 from .errors import BIMcloudBlobServerError, BIMcloudManagerError
 
 TS = time.time()
@@ -23,27 +16,25 @@ class Cloud:
 		self._session_id = None
 		self._user_id = None
 
-		self._model_server_urls = {}
 		self._blob_server_sessions = {}
 		self._next_revision_for_sync = 0
 
-		self.logtime = int(time.time())
+		self.iFolders = 0
+		self.iFiles = 0
+		self.iUsers = 0
 
 	def run(self):
 		try:
 			self.login()
+			self.db.logEntry()
 			self.fetchFolders()
-			# file = self._manager_api.get_resource(self._session_id, by_id='99A77C87-0EBC-460F-BB7D-779D454F0B98')
-			# print(file)
-			# print(datetime.now().strftime('%Y-%m-%d %H:%M:%S'))
-			# print(datetime.fromtimestamp(int(time.time())))
+			print(f'[{round((time.time() - TS),10)}]: Entry #{self.db.logId}: {self.iFolders} folders, {self.iFiles} files and {self.iUsers} joins recorded')
 		finally:
 			self.logout()
 
 	def login(self):
-		print(f'[{round((time.time() - TS),10)}]: Try to login as {self.username} ...')
 		self._user_id, self._session_id = self._manager_api.create_session(self.username, self._password, self.client_id)
-		print(f'[{round((time.time() - TS),10)}]: Logged in successfully')
+		print(f'[{round((time.time() - TS),10)}]: Logged as {self.username}')
 
 
 	def logout(self):
@@ -53,30 +44,17 @@ class Cloud:
 			api.close_session(session_id)
 		self._blob_server_sessions = {}
 		self._session_id = None
-		self._model_server_urls = {}
 		print(f'[{round((time.time() - TS),10)}]: Logged out')
 
-	def count(self, pid):
-		c = 0
-		try:
-			data = self._manager_api.get_resource(self._session_id, by_id=pid)
-			for d in data['members']:
-				c += 1
-				folder = self._manager_api.get_resource(self._session_id, by_id=d)
-				for f in folder['members']:
-					c += 1
-			print(f'[{round((time.time() - TS),10)}]: Total items - {c} found')
-			return(c)
-		except BIMcloudManagerError as err:
-			print(f'[{round((time.time() - TS),10)}]: {err}')
-
 	def fetchFolders(self, pid='projectRoot'):
+		print(f'[{round((time.time() - TS),10)}]: Retrieving resources ...')
 		try:
 			options = { 'sort-by': 'name' }
 			criterion = { '$eq': { '$parentId': pid } }
 			folders = self._manager_api.get_resources_by_criterion(self._session_id, criterion, options)
 			for f in folders:
-				self.db.addFolderData(f['id'], f['$parentId'], f['name'], self.logtime)
+				self.iFolders += 1
+				self.db.addFolderData(f['id'], f['$parentId'], f['name'])
 				self.fetchFiles(f['id'])
 		except BIMcloudManagerError as err:
 			print(f'[{round((time.time() - TS),10)}]: {err}')
@@ -87,6 +65,7 @@ class Cloud:
 			criterion = { '$eq': { '$parentId': pid } }
 			files = self._manager_api.get_resources_by_criterion(self._session_id, criterion, options)
 			for f in files:
+				self.iFiles += 1
 				lock = False
 				type = 10
 				if f['type'] == 'library':
@@ -97,13 +76,14 @@ class Cloud:
 					if f['access'] == 'locked':
 						lock = True
 					self.fetchUsers(f['$joinedUsers'], f['id'])
-				self.db.addFileDataData(f['id'], f['$parentId'], f['name'], type, f['$size'], lock, f['$modifiedDate']/1000, build, self.logtime)
+				self.db.addFileDataData(f['id'], f['$parentId'], f['name'], type, f['$size'], lock, f['$modifiedDate']/1000, build)
 		except BIMcloudManagerError as err:
 			print(f'[{round((time.time() - TS),10)}]: {err}')		
 
 	def fetchUsers(self, users, jfid):
 		try:
 			for u in users:
-				self.db.addUserData(u['id'], u['username'], u['name'], jfid, u['online'], round(u['lastActive']/1000,0), self.logtime)
+				self.iUsers += 1
+				self.db.addUserData(u['id'], u['username'], u['name'], jfid, u['online'], round(u['lastActive']/1000,0))
 		except:
 			print(f'[{round((time.time() - TS),10)}]: wrong user data')
--- lib/db.py
+++ lib/db.py
@@ -1,5 +1,6 @@
 import sqlite3
 import os
+import time
 
 class DB:
 
@@ -7,48 +8,74 @@ class DB:
 
 		self.path = "C:\\Users\\i.yurasov\\Desktop\\dev\\gs-bimcloud-automate\\stats.db"
 		self.con = None
+		self.logId = 0
+		self.logTime = 0
+		
 		self.connect();
+		self.logPrep();
 
 	def connect(self):
 		try:
 			con = sqlite3.connect(self.path)
 			con.execute(
 				'CREATE TABLE IF NOT EXISTS folders \
-				(id text, pid text, name text, logtime integer)'
+				(id text, pid text, name text, log integer)'
 			)
 			con.execute(
 				'CREATE TABLE IF NOT EXISTS files \
-				(id text, pid text, name text, type int, size real, lock bool, modified integer, build text, logtime integer)'
+				(id text, pid text, name text, type int, size real, lock bool, modified integer, build text, log integer)'
 			)
 			con.execute(
 				'CREATE TABLE IF NOT EXISTS users \
-				(id text, login text, name text, jfid text, online bool, spotted integer, logtime integer)'
+				(id text, login text, name text, jfid text, online bool, spotted integer, log integer)'
+			)
+			con.execute(
+				'CREATE TABLE IF NOT EXISTS entries \
+				(id integer primary key, logtime integer)'
 			)
 			self.con = con
 		except:
 			print('something wrong with connect')
 
 
-	def addFolderData(self, id, pid, name, logtime):
+	def logPrep(self):
+		try:
+			self.logTime = int(time.time())
+			c = self.con.cursor()
+			c.execute('SELECT MAX(id) FROM entries')
+			logId = c.fetchone()[0]
+			if logId is not None: self.logId = logId + 1
+		except:
+			print('something wrong while retrieving the log id')
+
+	def logEntry(self):
 		try:
 			c = self.con.cursor()
-			c.execute("INSERT INTO folders (id, pid, name, logtime) VALUES (?, ?, ?, ?)", (id, pid, name, logtime))
+			c.execute("INSERT INTO entries (id, logtime) VALUES (?, ?)", (self.logId, self.logTime))
+			self.con.commit()
+		except:
+			print('something wrong with entry logging')
+
+	def addFolderData(self, id, pid, name):
+		try:
+			c = self.con.cursor()
+			c.execute("INSERT INTO folders (id, pid, name, log) VALUES (?, ?, ?, ?)", (id, pid, name, self.logId))
 			self.con.commit()
 		except:
 			print('something wrong with folder insertion')
 
-	def addFileDataData(self, id, pid, name, type, size, lock, modified, build, logtime):
+	def addFileDataData(self, id, pid, name, type, size, lock, modified, build):
 		try:
 			c = self.con.cursor()
-			c.execute("INSERT INTO files (id, pid, name, type, size, lock, modified, build, logtime) VALUES (?, ?, ?, ?, ?, ?, ?, ?, ?)", (id, pid, name, type, size, lock, modified, build, logtime))
+			c.execute("INSERT INTO files (id, pid, name, type, size, lock, modified, build, log) VALUES (?, ?, ?, ?, ?, ?, ?, ?, ?)", (id, pid, name, type, size, lock, modified, build, self.logId))
 			self.con.commit()
 		except:
 			print('something wrong with file insertion')
 
-	def addUserData(self, id, login, name, jfid, online, spotted, logtime):
+	def addUserData(self, id, login, name, jfid, online, spotted):
 		try:
 			c = self.con.cursor()
-			c.execute("INSERT INTO users (id, login, name, jfid, online, spotted, logtime) VALUES (?, ?, ?, ?, ?, ?, ?)", (id, login, name, jfid, online, spotted, logtime))
+			c.execute("INSERT INTO users (id, login, name, jfid, online, spotted, log) VALUES (?, ?, ?, ?, ?, ?, ?)", (id, login, name, jfid, online, spotted, self.logId))
 			self.con.commit()
 		except:
 			print('something wrong with user insertion')
